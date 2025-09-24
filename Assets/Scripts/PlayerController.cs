d42aa182faa68978a5b9e94580f692ed77e2fd10
lyd <lydia.benostinane@universit-paris-saclay.fr>
2025-09-24 12:20:31 +0200

    player script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+    private bool isMoving;
+    private Vector2 input;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (!isMoving)
+        {
+            Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+       if(input != Vector2.zero)
+        {
+            var targetPos = transform.position;
+            targetPos.x += input.x;
+            targetPos.y += input.y;
+
+            StartCoroutine(Move(targetPos));
+        }
     }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+   
 }
