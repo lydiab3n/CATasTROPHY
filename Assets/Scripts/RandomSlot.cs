e2bd0e1fa2dc28e1a34edd836a129e07f6d34a78
lyd <lydia.benostinane@universit-paris-saclay.fr>
2025-09-28 00:04:15 +0200

    slot machine first try

---
+++ Assets/Scripts/RandomSlot.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomSlot : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    public float StopTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RandomingImage();
+
+    }
+
+    void RandomingImage()
+    {
+
+        gameObject.GetComponent<UnityEngine.UI.Image>().sprite = sprites[Random.Range(0, sprites.Length)];
+
+    }
+
+    void EndRand()
+    {
+        enabled = false;
+    }
+
+    public void StopRand()
+    {
+
+        Invoke("EndRand", StopTime);
+
+    }
+    public void StartRand()
+    {
+
+        enabled = true;
+    }
+}
