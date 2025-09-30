84aab21198cf66da78a9a5720c938642ffc700e2
lyd <lydia.benostinane@universit-paris-saclay.fr>
2025-09-30 10:33:24 +0200

    ui first try

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class NPCInteraction : MonoBehaviour
 {
-
+    public GameObject chatbox;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("NPC"))
         {
             Debug.Log("Collision with NPC");
+            chatbox.SetActive(true);
             // Do something
         }
     }
